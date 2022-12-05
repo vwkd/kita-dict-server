9f759bfa6c7f1c39a25dfa28693ee33a6e7b1188
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-05 15:17:29 +0200

    feat: make terms cursive

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ const input = data
   .slice(0, data.indexOf(`## ${UNTIL_PAGE}`))
   .replace(/^##.*/gm, "")
   .replace(/^\n/gm, "")
-  .replace(/\n♦︎ /g, "");
+  .replace(/\n♦︎ /g, "")
+  .replace(/(?<=^|[\( ])(([123]\.sg)|([123]\.pl)|(a)|(A)|(ad\.dem\.)|(ad\.int\.)|(ad\.rel\.)|(ad)|(aor)|(attr)|(cd)|(cj\.pr\.)|(cj\.f\.)|(cj\.pt\.)|(cj)|(comp)|(D\/A)|(dekl)|(dim)|(E)|(enkl)|(fig)|(fut)|(f\/sg)|(f\/pl)|(f)|(G)|(3\.Gr\.)|(HV)|(I)|(imp)|(impf)|(inf)|(int)|(interrog\. Possessivpron\.)|(Indefinitpron\.)|(indefinites Possessivpron\.)|(m\/sg)|(m\/pl)|(m)|(N)|(n\/sg)|(n\/pl)|(n)|(opt\.pr)|(opt\.fut)|(opt)|(p\.a\.)|((perf\.))|(p\.f\.)|(p\.n\.)|(p\.p\.)|(pf)|(pl-pf)|(pl)|(pp)|(pp mit G)|(pr\.dem\.)|(pr\.int\.)|(pr\.pers\.)|(pr\.poss\.)|(pr\.rel)|(pr)|(prv)|(rel\. Possessivpron\.)|(S)|(sg)|(spn)|(sub)|(sup)|(V))(?=[ \),;]|$)/gm, "*$1*");
 
 const entries = input.split(/\n(?!  )/);
 
